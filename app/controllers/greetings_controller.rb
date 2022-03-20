b87b67878b175d8899973ef65684f745154d6f6b
Ondrej Svoboda <admin@svoby.eu>
2022-03-20 17:09:06 +0100

    greeting validation

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,5 +1,5 @@
 class GreetingsController < ApplicationController
-  before_action :set_greeting, only: %i[ show edit update destroy ]
+  before_action :set_greeting, only: [:show, :edit, :update, :destroy]
 
   # GET /greetings or /greetings.json
   def index
